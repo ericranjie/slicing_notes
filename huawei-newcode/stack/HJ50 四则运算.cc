17c29bee4800c3228bf9f2ae87b23e69e7f5cbe6
ericran <ericranjie1991@gmail.com>
2024-11-02 10:45:58 +0800

    Add huawei newcode.

--- huawei-newcode/stack/HJ50 四则运算.cc
+++ huawei-newcode/stack/HJ50 四则运算.cc
@@ -26,45 +26,71 @@ bool is_rbracket(char c) {
     return (c == '}') || (c == ')') || (c == ']');
 }
 
-// POE: priority-of-operator: -4/(8-6)+7
-int comp(int a, int b, char o) { // compute
-    cout << o << " " << to_string(a) << " " << to_string(b) << endl;
-    if (o == '+') {
-        return a + b;
-    } else if (o == '-') {
-        return b - a; // POE: minus-order
-    } else if (o == '*') {
-        return a * b;
-    } else if (o == '/') {
-        return b / a; // POE: same-as-upper
+// recite:
+int compute(string t) {
+    // cout << t << endl;
+    // stack<int> nst; // number-stack
+    int i = 0;
+    int c = 0; // current-number-value
+    int cur_res = 0;
+    char op = '+'; // POE:
+    int res = 0;
+    while (i < t.size()) {
+        if (is_number(t[i])) { // is-number
+            c *= 10;
+            c += t[i] - '0';
+        } else if (is_lbracket(t[i])) { // left-bracket
+            int cnt = 0; // valid-pathesis-count
+            int j = 0;
+            for (j = i; j < t.size(); j++) {
+                if (is_lbracket(t[j])) {
+                    ++cnt;
+                } else if (is_rbracket(t[j])) {
+                    --cnt;
+                }
+                if (cnt == 0) {
+                    break;
+                }
+            }
+            c = compute(t.substr(i + 1, j - i - 1)); // POE:
+            i = j; // POE: not+1
+        }
+        // cout << "c: " << c << " t[i]: " << t[i] << " curres: "
+        //     << cur_res << endl;
+        if (is_operator(t[i]) || i == t.size() - 1) {
+            char to = t[i]; // tmp-operator
+            if (op == '+') {
+                cur_res += c;
+            } else if (op == '-') {
+                cur_res -= c;
+            } else if (op == '*') {
+                cur_res *= c;
+            } else if (op == '/') {
+                cur_res /= c;
+            }
+            // cout << "curres: " << cur_res << " c: " << c
+            //     << " t[i]: " << t[i] << endl;
+            if (t[i] == '+' || t[i] == '-' || i == t.size() - 1) { // POE:
+                res += cur_res;
+                cur_res = 0;
+            }
+            op = to; // renew-operator
+            c = 0;
+        }
+        ++i;
     }
-    return 1; // error
+
+    // cout << ": " << res << endl;
+    return res;
 }
 
+// recite:
 int main() {
     string t;
     getline(cin, t);
 
-    stack<char> ost; // operator-stack
-    stack<int> nst; // number-stack
-
-    bool m = false; // minus-flag
-    for (int i = 0; i < t.size(); i++) {
-        if (is_number(t[i])) { // number
-            int n = t[i] - '0';
-            if (m) {
-                n *= (-1);
-            }
-            nst.push(n);
-            m = false;
-        }
-    } else if (is_operator(t[i])// operator
-        is_lbracket(t[i])) { // left-bracket
-        ost.push(t[i]);
-    } else if (is_rbracket(t[i])) { // right-bracket
-        
-        continue;
-    }
+    int r = compute(t);
+    cout << r << endl;
 
     return 0;
 }
