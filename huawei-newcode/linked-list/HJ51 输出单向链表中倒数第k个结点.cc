8e95bda1014d129ef38136e9670f1505c917c95a
ericran <ericranjie1991@gmail.com>
2024-11-05 17:55:16 +0800

    Add huawei newcode.

--- huawei-newcode/linked-list/HJ51 输出单向链表中倒数第k个结点.cc
+++ huawei-newcode/linked-list/HJ51 输出单向链表中倒数第k个结点.cc
@@ -9,66 +9,69 @@ struct ListNode {
     ListNode* m_pNext;
 };
 
-// bug:
-int main(int argc, char *argv[]) {
-  int num, k;
-  vector<int> values;
-  ListNode *head = nullptr,
+void helper(int n, vector<int> v, int k) {
+    ListNode *head = nullptr,
     *dummy = nullptr,
     *pre = nullptr;
 
-  dummy = (ListNode *)malloc(sizeof(ListNode));
-  dummy->m_nKey = -1;
-  dummy->m_pNext = nullptr;
+    dummy = (ListNode *)malloc(sizeof(ListNode));
+    dummy->m_nKey = -1;
+    dummy->m_pNext = nullptr;
 
-  cin >> num;
-  cin.ignore(256, '\n'); // POE:
+    pre = dummy;
 
-  pre = dummy;
+    for (int i = 0; i < n; i++) {
+        ListNode *cur = nullptr;
+        cur = (ListNode *)malloc(sizeof(ListNode));
+        cur->m_nKey = v[i];
+        cur->m_pNext = nullptr;
 
-  for (int i = 0; i < num; i++) {
-    int tmp;
-    cin >> tmp;
-    cin.ignore(256, '\n'); // POE:
-    values.push_back(tmp);
-    // ListNode *cur = new ListNode(tmp, nullptr); // POE:
-    ListNode *cur = nullptr;
-    cur = (ListNode *)malloc(sizeof(ListNode));
-    cur->m_nKey = tmp;
-    cur->m_pNext = nullptr;
+        pre->m_pNext = cur;
+        pre = pre->m_pNext;
+    }
 
-    pre->m_pNext = cur;
-    pre = pre->m_pNext;
-  }
+    ListNode *fast = dummy->m_pNext,
+        *slow = dummy->m_pNext;
 
-  // for (int i = 0; i < num; i++) {
-  //   cout << values[i] << endl;
-  // }
-  // ListNode *cur = dummy->m_pNext;
-  // while (cur != nullptr) {
-  //   cout << cur->m_nKey << endl;
-  //   cur = cur->m_pNext;
-  // }
+    // cout << "k: " << k << endl;
+    while (k > 0) {
+        k--;
+        fast = fast->m_pNext;
+    }
 
-  cin >> k;
-  cin.ignore(256, '\n'); // POE:
+    while (fast && slow) {
+        fast = fast->m_pNext;
+        slow = slow->m_pNext;
+    }
 
-  // cout << "k = " << k << endl;
+    cout << slow->m_nKey << endl;
+    return;
+}
 
-  ListNode *fast = dummy->m_pNext,
-    *slow = dummy->m_pNext;
+int main(int argc, char *argv[]) {
+    vector<vector<int>> vs; // values
+    vector<int> ns; // nums
+    vector<int> ks; // ks
 
-  while (k > 0) {
-    k--;
-    fast = fast->m_pNext;
-  }
+    int t;
+    while (cin >> t) {
+        ns.push_back(t);
 
-  while (fast && slow) {
-    fast = fast->m_pNext;
-    slow = slow->m_pNext;
-  }
+        int n = t;
+        vector<int> o;
+        for (int i = 0; i < n; i++) {
+            cin >> t;
+            o.push_back(t);
+        }
+        vs.push_back(o);
 
-  cout << slow->m_nKey << endl;
+        cin >> t;
+        ks.push_back(t);
+    }
 
-  return 0;
+    for (int i = 0; i < vs.size(); ++i) {
+        helper(ns[i], vs[i], ks[i]);
+    }
+
+    return 0;
 }
