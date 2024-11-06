b219554d02c7b337ae44ca69e9541437653a97af
ericran <ericranjie1991@gmail.com>
2024-11-06 12:37:30 +0800

    Add huawei newcode.

---
+++ huawei-newcode/math/HJ82 将真分数分解为埃及分数.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+// recite:special
+// b = a * p + r
+// a / b =
+//  a / a * p + r =
+// 
+//  1/(p+1) + (a-r)/(a*p + r)(p + 1)
+
+void solve(string t) {
+    int a = 0, b = 0, n = 0;
+    for (int i = 0; i < t.size(); i++) {
+        if (t[i] == '/') {
+            a = n;
+            // cout << "a = " << a << endl;
+            n = 0;
+            i++;
+        }
+        n *= 10;
+        n += (t[i] - '0');
+    }
+    b = n;
+    // cout << "b = " << n << endl;
+
+    int d, m;
+
+    while (true) {
+        d = b / a;
+        m = b % a;
+        cout << "1/" << to_string(d + 1) << "+";
+        int na = a - m;
+        int nb = b * (d + 1);
+        if (na == 1 || 
+            (na > 1 && nb % na == 0)) {
+            cout << to_string(na) << "/" << to_string(nb);
+            break;
+        } else {
+            a = na;
+            b = nb;
+        }
+    }
+    cout << endl;
+    return;
+}
+
+int main() {
+    string t;
+    while (getline(cin, t)) {
+        // cout << t << endl;
+        solve(t);
+    }
+    return 0;
+}
+// 64 位输出请用 printf("%lld")
