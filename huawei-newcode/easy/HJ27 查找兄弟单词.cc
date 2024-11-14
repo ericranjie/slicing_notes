110cf9a3341cf20cbdf747b4231aebed765d27fd
ericran <ericranjie1991@gmail.com>
2024-11-14 15:29:20 +0800

    Add huawei newcode.

--- huawei-newcode/easy/HJ27 查找兄弟单词.cc
+++ huawei-newcode/easy/HJ27 查找兄弟单词.cc
@@ -3,106 +3,59 @@
 #include <algorithm>
 using namespace std;
 
-#if 1
-bool veri(string a, string b) { // verify: same-size POE:
-    // cout << a << " " << b << endl;
-    char va[26] = {0}, vb[26] = {0};
-    if (a == b) { // POE:
+bool veri(string a, string b) { // Verify: same-size; POE:
+    if (a == b) { // Guard: Brother word must not same; POE:
         return false;
     }
 
+    char va[26] = {0}, vb[26] = {0};
     for (int i = 0; i < a.size(); i++) {
         ++va[a[i] - 'a'];
         ++vb[b[i] - 'a'];
     }
 
-    for (int i = 0; i < 26; i++) {
-        if (va[i] != vb[i]) {
-            return false;
-        }
-    }
+    for (int i = 0; i < 26; i++)
+        if (va[i] != vb[i]) return false;
 
-    // cout << "--- " << a << " " << b << endl;
     return true;
 }
-#endif
-#if 0
-bool veri(string s1, string s2){  //查看是否是兄弟单词 // POE:
-    if (s1.length() == s2.length()) { //兄弟单词一定要长度相等
-        if(s1 == s2) //不能是同一个
-            return false;
-        sort(s1.begin(), s1.end());  //对两个字符串按字符字典序排序
-        sort(s2.begin(), s2.end());
-        if(s1 == s2) //排序后一样才是改变位置能办到的
-            return true;
-    }
-    return false;
-}
-#endif
+
 int main() {
     string t;
     int n, k;
     cin >> n;
 
-    vector<string> rst; // dict-set-raw // POE:
+    vector<string> r; // Raw dictionary; // POE:
     for (int i = 0; i < n; i++) {
         cin >> t;
-        rst.push_back(t);
-        // cout << t << endl;
+        r.push_back(t);
     }
-    cin >> t; // raw-string
-    // cout << t;
-    cin >> k; // k-index
-    // cout << k;
+    cin >> t; // Target string;
+    cin >> k; // K index;
 
-    vector<string> dst; // dict-set
-    for (auto it = rst.begin(); it != rst.end(); it++) {
+    vector<string> dst; // Dictionary set
+    for (auto it = r.begin(); it != r.end(); it++) {
         string tmp = *it;
-        // cout << tmp << " " << tmp.size() << " " << t.size();
-        if (tmp.size() != t.size()) { // char-num-not-same
-            // rst.erase(it); // POE:
-            // ++it;
-            // cout << tmp << " ";
+        if (tmp.size() != t.size()) // Guard: Character number not same;
             continue;
-        } else { // char-num-same
-            if (!veri(t, tmp)) { // POE:
+         else  // Main: Character number same;
+            if (!veri(t, tmp))  // POE:
                 continue;
-            }
-            // set<char> cst(t.begin(), t.end()); // char-set
-            // set<char> cst2(tmp.begin(), tmp.end());
-            // if (cst != cst2) {
-            //     continue;
-            // }
-            // for (int i = 0; i < tmp.size(); i++) {
-            //     if (!cst.count(tmp[i])) {
-            //         continue;
-            //     }
-            // }
-        }
+        
         dst.push_back(*it);
-        // cout << *it << " ";
     }
-    sort(dst.begin(), dst.end()); // POE: no-need
- 
+
+    sort(dst.begin(), dst.end()); // POE: no-need 
     cout << dst.size() << endl;
 
     for (auto it = dst.begin(); it != dst.end(); ++it) {
-    // for (auto it : dst) {
-        // cout << k << endl;
-        // cout << *it << " ";
         string tmp = *it; // POE:
-        // if (dst.count(t)) {
-        //     continue;
-        // }
         --k;
-        
-        if (k == 0) {
-            cout << *it;
-        }
+        if (k == 0) cout << *it;
     }
-
     return 0;
 }
+
 // 3 abc bca cab abc 1
 // 2
 // bca
@@ -111,4 +64,3 @@ int main() {
 // adbcb
 // 106
 // aabad
-// 64 位输出请用 printf("%lld")
