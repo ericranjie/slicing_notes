e94f9e21697ff0b55af990a1fc6b6f4550d660e4
ericran <ericranjie1991@gmail.com>
2024-11-14 16:53:14 +0800

    Add huawei newcode.

--- huawei-newcode/complex_step/HJ33 整数与IP地址间的转换.cc
+++ huawei-newcode/complex_step/HJ33 整数与IP地址间的转换.cc
@@ -17,9 +17,11 @@ int main() {
     string t;
     long long d, r = 0; // POE: int-d-too-short
     getline(cin, t);
-    cin >> d;
+    cin >> d; // second-input: dex-IP
 
-    string o;
+
+    // Step-1: Input;
+    string o; // current-one
     for (int i = 0; i < t.size(); i++) {
         if (t[i] != '.') {
             o.push_back(t[i]);
@@ -30,51 +32,45 @@ int main() {
         }
     }
 
-    // transfer-to-dex:
+
+    // Step-2: Transfer to dex;
     string t1;
     int dex[4];
     v.push_back(o);
     for (int i = 0; i < v.size(); i++) {
-        // cout << v[i] << endl;
-        o = v[i];
+        o = v[i]; // one-string
         r = 0; // one-dex
         for (int j = 0; j < o.size(); j++) {
             char c = o[j];
             r *= 10; // POE: dont-reverse-with-down-code
             r += (c - '0');
         }
-        // cout << r << endl;
         dex[i] = r;
     }
     r = 0; // reuse-result
-    // for (int i = 0; i < 4; i++) {
-        // cout << dex[i] << " ";
     r = dex[0] * pow(2, 24) + dex[1] * pow(2, 16) + dex[2] * pow(2, 8)
         + dex[3] * pow(2, 0);
     cout << r << endl;
-    // }
-    
-    // solve-second-input:
+
+
+    // Step-3: Solve second input(IP: dex to string);
     int a = 0;
-    // highest
     a = d / pow(2, 24);
-    // cout << d << endl;
     cout << to_string(a) << ".";
-    //
+
     d = d - a * pow(2, 24);
     a = d / pow(2, 16);
     cout << to_string(a) << ".";
-    //
+
     d = d - a * pow(2, 16);
     a = d / pow(2, 8);
     cout << to_string(a) << ".";
-    //
+
     d = d - a * pow(2, 8);
     a = d / pow(2, 1);
     cout << to_string(d);
-
     return 0;
 }
+
 // 39.66.68.72
 // 3868643487
-// 64 位输出请用 printf("%lld")
