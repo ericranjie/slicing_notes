f2720385f3f5c39eb5dc8a0084ac0196c1594df5
ericran <ericranjie1991@gmail.com>
2024-11-16 18:05:28 +0800

    Add huawei newcode.

--- huawei-newcode/string/HJ88 扑克牌大小.cc
+++ huawei-newcode/string/HJ88 扑克牌大小.cc
@@ -8,120 +8,99 @@ using namespace std;
 // 炸弹（四个）
 // 对王
 
+// need-reveal: complex
 int trans(string t) {
     if (t.size() == 1) {
-        if (t[0] >= '0' && t[0] <= '9') {
+        if (t[0] >= '0' && t[0] <= '9')
             return t[0] - '0';
-        } else if (t[0] == 'J') {
+        else if (t[0] == 'J')
             return 11;
-        } else if (t[0] == 'Q') {
+        else if (t[0] == 'Q')
             return 12;
-        } else if (t[0] == 'K') {
+        else if (t[0] == 'K')
             return 13;
-        } else if (t[0] == 'A') {
+        else if (t[0] == 'A')
             return 14;
-        } else if (t[0] == '2') {
+        else if (t[0] == '2')
             return 15;
-        } else if (t[0] == 'o') {
+        else if (t[0] == 'o')
             return 16;
-        } else if (t[0] == 'O') {
+        else if (t[0] == 'O')
             return 17;
-        }
-    } else if (t.size() == 2) {
-        if (t[0] == '1' && t[1] == '0') {
+    } else if (t.size() == 2)
+        if (t[0] == '1' && t[1] == '0')
             return 10;
-        }
-    }
     return 0;
 }
 
 vector<string> extract(string t) {
-    vector<string> r; // result
+    vector<string> r; // r: result
 
-    int s = 0;
+    int s = 0; // s: seperate
     for (int i = 0; i < t.size(); i++) {
-        if (t[i] != ' ') {
-
-        } else {
-            if (t.substr(s, i - s) == "joker") {
+        if (t[i] == ' ') {
+            if (t.substr(s, i - s) == "joker")
                 r.push_back("o");
-            } else if (t.substr(s, i - s) == "JOKER") {
+            else if (t.substr(s, i - s) == "JOKER")
                 r.push_back("O");
-            } else {
+            else
                 r.push_back(t.substr(s, i - s)); // POE:
-            }
-            // cout << t.substr(s, i - s) << " ";
             s = i + 1;
         }
     }
-    if (t.substr(s, t.size() - s) == "joker") {
+    if (t.substr(s, t.size() - s) == "joker")
         r.push_back("o");
-    } else if (t.substr(s, t.size() - s) == "JOKER") {
+    else if (t.substr(s, t.size() - s) == "JOKER")
         r.push_back("O");
-    } else {
+    else
         r.push_back(t.substr(s, t.size() - s));
-    }
     return r;
 }
 
 int main() {
     string t;
     getline(cin, t);
-    int s = 0;
+
+    // Step-1: Parse input;
+    int s = 0; // s: seperate
     for (int i = 0; i < t.size(); i++) {
         if (t[i] == '-') {
             s = i;
             break;
         }
     }
-        string a = t.substr(0, s);
-        string b = t.substr(s + 1, t.size() - s);
-        // cout << a << endl << b << endl;
-        vector<string> va, vb;
-        va = extract(a);
-        vb = extract(b);
-        int m = va.size();
-        int n = vb.size();
+    string a = t.substr(0, s);
+    string b = t.substr(s + 1, t.size() - s);
+    vector<string> va, vb;
+    va = extract(a);
+    vb = extract(b);
+    int m = va.size();
+    int n = vb.size();
 
-        // cout << m << " " << n << endl;
-        if (m != n) {
-            // cout << va[0] << " " << vb[0] << endl;
-            if (va[0] == "o") {
-                cout << a << endl;
-            } else if (vb[0] == "o") {
-                cout << b << endl;
-            } else if (m == 4) {
-                cout << a << endl;
-            } else if (n == 4) {
-                cout << b << endl;
-            } else {
-                cout << "ERROR" << endl;
-            }
-        }
 
-        if (m == 2 && n == 2) { // double-or-joker
-                if (trans(va[0]) > trans(vb[0])) {
-                    cout << a << endl;
-                } else {
-                    cout << b << endl;
-                }
-            // }
-        } else if ((m == 1 && n == 1) || (m == 3 && n == 3)) {
-            if (trans(va[0]) > trans(vb[0])) {
-                cout << a << endl;
-            } else {
-                cout << b << endl;
-            }
-        } else if ((m == 5 && n == 5) || (m == 4 && n == 4)) {
-            if (trans(va[0]) > trans(vb[0])) {
-                cout << a << endl;
-            } else {
-                cout << b << endl;
-            }
-        }
-
-        return 0;
+    // Step-2: not equal length;
+    if (m != n) {
+        if (va[0] == "o") cout << a << endl;
+        else if (vb[0] == "o") cout << b << endl;
+        else if (m == 4) cout << a << endl;
+        else if (n == 4) cout << b << endl;
+        else cout << "ERROR" << endl;
     }
+
+
+    // Step-3: double or joker;
+    if (m == 2 && n == 2) {
+        if (trans(va[0]) > trans(vb[0])) cout << a << endl;
+        else cout << b << endl;
+    } else if ((m == 1 && n == 1) || (m == 3 && n == 3)) {
+        if (trans(va[0]) > trans(vb[0])) cout << a << endl;
+        else cout << b << endl;
+    } else if ((m == 5 && n == 5) || (m == 4 && n == 4)) {
+        if (trans(va[0]) > trans(vb[0])) cout << a << endl;
+        else cout << b << endl;
+    }
+    return 0;
+}
 
 // A-10
 // joker-4
--- huawei-newcode/string/HJ90 合法IP.cc
+++ huawei-newcode/string/HJ90 合法IP.cc
@@ -3,17 +3,18 @@
 using namespace std;
 
 // need-reveal:many-entrap
-void veri(string t) { // verify
-    int n = 0, nn = 0, nd = 0; // number-of-num & dot
-
-    bool nc = false; // number-continue
-    int la = 0;
+void veri(string t) {
+    int n = 0, nn = 0, nd = 0; // nn: number-of-num & nd: dot
+    bool nc = false; // nc: number-continue
+    int la = 0; // la: left-character
     for (int i = 0; i < t.size(); i++) {
+        // Must not have +/-;
         if (t[i] == '+' || t[i] == '-') { // POE:
             cout << "NO" << endl;
             return;
         }
-        if (t[i] >= '0' && t[i] <= '9') {
+
+        if (t[i] >= '0' && t[i] <= '9') { // is number
             n *= 10;
             n += (t[i] - '0');
             if (!nc) {
@@ -21,7 +22,7 @@ void veri(string t) { // verify
                 la = t[i] - '0';
             }
             nc = true;
-        } else if (t[i] == '.') {
+        } else if (t[i] == '.') { // is dot
             ++nd; nc = false;
             if (n > 255) {
                 cout << "NO" << endl;
@@ -39,10 +40,14 @@ void veri(string t) { // verify
         cout << "NO" << endl;
         return;
     }
+
+    // Number of dot must = 3;
     if (nd != 3) {
         cout << "NO" << endl;
         return;
     }
+
+    // Number of number must = 4;
     if (nn != 4) {
         cout << "NO" << endl;
         return;
