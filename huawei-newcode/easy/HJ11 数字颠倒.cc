2fc77af14b5ecde1e86a5f5582c4d6fbf0c0992e
ericran <ericranjie1991@gmail.com>
2024-11-17 13:59:45 +0800

    Add huawei newcode.

--- huawei-newcode/easy/HJ11 数字颠倒.cc
+++ huawei-newcode/easy/HJ11 数字颠倒.cc
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main() {
-    int n, m = 0, q = 0;
+    int n, m = 0; // m: module
     cin >> n;
 
-    if (n == 0) cout << "0"; // POE:
+    if (n == 0) cout << "0"; // Guard: POE:
 
     while (n > 0) {
         m = n % 10;
         cout << to_string(m);
         n = n / 10;
-        // q = n / 10;
     }
 
     return 0;
 }
-// 64 位输出请用 printf("%lld")
