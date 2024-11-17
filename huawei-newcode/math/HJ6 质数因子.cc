f6064c5360f6856a5532dbc4d23d6a1dd9a5093a
ericran <ericranjie1991@gmail.com>
2024-11-17 18:58:43 +0800

    Add huawei newcode.

--- huawei-newcode/math/HJ6 质数因子.cc
+++ huawei-newcode/math/HJ6 质数因子.cc
@@ -2,14 +2,12 @@
 #include <cmath>
 using namespace std;
 
-// resite:
+// need-resite:
 int main() {
     int n;
     cin >> n;
 
     int k = sqrt(n);
-    // cout << n << endl;
-
     for (int i = 2; i < k + 1; i++) {
         while (n % i == 0) {
             cout << i << " ";
@@ -17,19 +15,9 @@ int main() {
         }
     }
 
-    if (n != 1) { // POE:
+    if (n != 1) // POE:
         cout << n;
-    }
-    // int i = 2;
-    // while (n >= 1) {
-    //     int quo = n / i;
-    //     int mod = n % i;
-    //     if (mod == 0) {
-    //         cout << i << " ";
-    //     }
-    // }
-
     return 0;
 }
+
 // 64577
-// 64 位输出请用 printf("%lld")
