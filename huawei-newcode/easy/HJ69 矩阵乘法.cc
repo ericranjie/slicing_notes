50576949d7e1f4e997f45e14670be144c8f19091
ericran <ericranjie1991@gmail.com>
2024-11-17 13:38:44 +0800

    Add huawei newcode.

--- huawei-newcode/easy/HJ69 矩阵乘法.cc
+++ huawei-newcode/easy/HJ69 矩阵乘法.cc
@@ -10,35 +10,28 @@ int main() {
         b(y, vector<int>(z, 0)),
         c(x, vector<int>(z, 0));
 
-    // cout << x << y << z;
+    // Step-1: Input matrix a, b;
     for (int i = 0; i < x; i++) {
         for (int j = 0; j < y; j++) {
             cin >> t;
             a[i][j] = t;
-            // cout << t << " ";
         }
-        // cout << endl;
     }
-
     for (int i = 0; i < y; i++) {
         for (int j = 0; j < z; j++) {
             cin >> t;
             b[i][j] = t;
-            // cout << t << " ";
         }
-        // cout << endl;
     }
 
+    // Step-2: Multiple matrix c;
     for (int i = 0; i < x; i++) {
         for (int j = 0; j < z; j++) {
-            for (int k = 0; k < y; k++) {
+            for (int k = 0; k < y; k++)
                 c[i][j] += a[i][k] * b[k][j];
-            }
             cout << c[i][j] << " ";
         }
         cout << endl;
     }
-
     return 0;
 }
-// 64 位输出请用 printf("%lld")
