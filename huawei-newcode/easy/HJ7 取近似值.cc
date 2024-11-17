70c518ec8c9a5be3a4875c31a59843f62fa94e58
ericran <ericranjie1991@gmail.com>
2024-11-17 19:11:48 +0800

    Add huawei newcode.

--- huawei-newcode/easy/HJ7 取近似值.cc
+++ huawei-newcode/easy/HJ7 取近似值.cc
@@ -2,14 +2,15 @@
 #include <unistd.h>
 using namespace std;
 
+// need-familiar: math;
 int main(int argc, char *argv[]) {
     double f;
     cin >> f;
 
     f *= 10;
     int f_ = (int)f;
-    int mod = f_ % 10;
-    int quo = f_ / 10;
+    int mod = f_ % 10; // mod: module
+    int quo = f_ / 10; // quo: quotient
     if (mod >= 5)
         cout << quo + 1 << endl;
     else
