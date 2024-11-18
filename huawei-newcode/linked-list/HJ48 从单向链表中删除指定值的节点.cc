cf7116b460f98c0247bac3fa229e369be7da93bd
eric.ran <ericranjie1991@gmail.com>
2024-11-18 16:49:26 +0800

    Modify details.

--- huawei-newcode/linked-list/HJ48 从单向链表中删除指定值的节点.cc
+++ huawei-newcode/linked-list/HJ48 从单向链表中删除指定值的节点.cc
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+// need-familiar:
 // many-entrap:
 struct node {
     int value;
@@ -9,26 +10,23 @@ struct node {
 };
 
 int main() {
-    int n, h, t; // number-head-target
+    int n, h, t; // n-h-t: number head target
     vector<int> v;
 
+    // Step-1: Input;
     cin >> n >> h;
-    while (cin >> t) {
-        v.push_back(t);
-        // cout << t << " ";
-    }
+    while (cin >> t) v.push_back(t);
     t = v[v.size() - 1]; // target
     v.pop_back();
-    if (v.size() % 2 != 0) {
-        return 0; // guard: error
-    }
+    if (v.size() % 2 != 0) // Guard: error
+        return 0;
 
-    // header-construct:
+    // Step-2: Header construct;
     struct node *ph = (struct node *)malloc(sizeof(struct node));
     ph->value = h;
     ph->p_next = nullptr;
 
-    // body-construct:
+    // Step-3: Body construct;
     bool pair = false; // construct-one-pair
     int o = 0, e = 0; // odd-even
     for (int i = 0; i < v.size(); i++) {
@@ -41,18 +39,15 @@ int main() {
         }
         
         if (pair) { // found-a-pair(odd->even)
-            // cout << e << "-" << o<< endl;
             pair = false; // POE:
             struct node *c = ph; // current-pointer
             while (c != nullptr) {
-                // cout << c->value << "--" << e << endl;
                 // POE: insert-need-verify
                 if (c->value == o) { // POE: second-one-is-odd
                     struct node *p = (struct node *)malloc(sizeof(struct node));
                     p->value = e; // POE:
                     p->p_next = c->p_next;
                     c->p_next = p;
-                    // cout << o << "->" << e << endl;
                     break;
                 }
                 c = c->p_next;
@@ -60,12 +55,11 @@ int main() {
         }
     }
 
-    // step-n: find-node-and-erase:
+    // Step-n: Find node and erase;
     struct node *dummy = (struct node *)malloc(sizeof(struct node));
     dummy->value = -1;
     dummy->p_next = ph;
     struct node *c = dummy;
-    // cout << t << endl;
     while (c->p_next) {
         if (c->p_next->value == t) {
             c->p_next = c->p_next->p_next;
@@ -82,4 +76,3 @@ int main() {
     return 0;
 }
 // 5 2 3 2 4 3 5 2 1 4 3
-// 64 位输出请用 printf("%lld")
